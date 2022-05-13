2fbb5d8ff2f6efe9155f8d6bcf8620d7ca843e7b
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-05-13 15:53:42 -0300

    setup: Iniciando o projeto TrybeSmith

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import app from './app';
+import 'dotenv/config';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
-));
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on PORT: ${PORT}`));
 
 export default server;
