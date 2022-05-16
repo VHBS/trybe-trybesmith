c0d0f3eb449c1edbb286090896528b5cc9b04bb3
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-05-16 10:13:31 -0300

    fix: User Service responsavel pelo jwt

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,8 +1,6 @@
 import { ResultSetHeader } from 'mysql2/promise';
-import jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces/users';
 import connection from './connection';
-import jwtConfig from '../jwt';
 
 export default class UserModel {
   public connection;
@@ -11,14 +9,12 @@ export default class UserModel {
     this.connection = connection;
   }
 
-  public create = async ({ username, classe, level, password }: IUser): Promise<string> => {
+  public create = async ({ username, classe, level, password }: IUser): Promise<number> => {
     const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)', 
       [username, classe, level, password],
     );
 
-    const token = jwt.sign({ data: { username, id: insertId } }, 'trybesmith', jwtConfig);
-
-    return token;
+    return insertId;
   };
 }
--- src/services/users.ts
+++ src/services/users.ts
@@ -1,5 +1,7 @@
+import jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces/users';
 import UserModel from '../models/users';
+import jwtConfig from '../jwt';
 
 export default class UserService {
   public model: UserModel;
@@ -9,7 +11,10 @@ export default class UserService {
   }
 
   public create = async ({ username, classe, level, password }: IUser): Promise<string> => {
-    const token = await this.model.create({ username, classe, level, password });
+    const insertId = await this.model.create({ username, classe, level, password });
+
+    const token = jwt.sign({ data: { username, id: insertId } }, 'trybesmith', jwtConfig);
+
     return token;
   };
 }
